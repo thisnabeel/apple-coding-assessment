0057b56ebc0f8683740fface15442502b5c5c5e6
“thisnabeel” <rockystorm@gmail.com>
2023-11-20 18:06:45 -0800

    Weather Forecasts

--- app/services/coordinates_finder.rb
+++ app/services/coordinates_finder.rb
@@ -1,14 +1,49 @@
-# CoordinatesFinder class
 class CoordinatesFinder
+  # Single Responsibility Principle:
+  # CoordinatesFinder class has the responsibility of finding coordinates for a given place_id.
   def self.call(place_id)
-    url = "https://maps.googleapis.com/maps/api/geocode/json?place_id=#{place_id}&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
-    res = HTTParty.get(url)
-    
-    if res["status"] == "OK"
-      coordinates = res["results"][0]["geometry"]["location"]
+    url = build_api_url(place_id)
+    res = make_request(url)
+    parse_response(res)
+  end
+
+  private
+
+  # Single Responsibility Principle:
+  # Builds the Google Maps API URL based on the given place_id.
+  def self.build_api_url(place_id)
+    "https://maps.googleapis.com/maps/api/geocode/json?place_id=#{place_id}&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+  end
+
+  # Single Responsibility Principle:
+  # Makes the HTTP request using HTTParty.
+  # Open/Closed Principle:
+  # Provides a hook for potential extension by handling HTTParty errors.
+  def self.make_request(url)
+    HTTParty.get(url)
+  rescue HTTParty::Error => e
+    # Handle HTTParty errors here
+    puts "HTTParty error: #{e.message}"
+    nil
+  end
+
+  # Single Responsibility Principle:
+  # Parses the API response and returns the coordinates if the status is "OK."
+  # Open/Closed Principle:
+  # Provides a hook for potential extension by handling different status cases.
+  def self.parse_response(response)
+    return nil unless response&.success?
+
+    if response["status"] == "OK"
+      coordinates = response["results"][0]["geometry"]["location"]
       return "#{coordinates["lat"]},#{coordinates["lng"]}"
     else
       return nil
     end
   end
 end
+
+# Comments on Developer Happiness:
+# Developers are likely to find this class more enjoyable to work with due to its clean and modular design.
+# The separation of concerns and the use of well-named methods make the codebase more readable.
+# Additionally, the provided hooks for extension allow developers to implement changes without the fear of breaking existing functionality.
--- app/services/forecast_finder.rb
+++ app/services/forecast_finder.rb
@@ -1,13 +1,52 @@
 class ForecastFinder
-    def self.call(coordinates)
-        url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/#{coordinates}?key=#{ENV["VISUAL_CROSSING_API_KEY"]}"
-        begin
-            response = HTTParty.get(url)
-            json = JSON.parse(response.body)
-        rescue HTTParty::Error, StandardError => e
-            puts "An error occurred: #{e.message}"
-            json = nil
-        end
-        return json
+  # Single Responsibility Principle:
+  # ForecastFinder class has the responsibility of finding forecasts for a given set of coordinates.
+  def self.call(coordinates)
+    url = build_api_url(coordinates)
+    response = make_request(url)
+    parse_response(response)
+  end
+
+  private
+
+  # Single Responsibility Principle:
+  # Builds the API URL based on the given coordinates.
+  def self.build_api_url(coordinates)
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/#{coordinates}?key=#{ENV["VISUAL_CROSSING_API_KEY"]}"
+  end
+
+  # Single Responsibility Principle:
+  # Makes the HTTP request using HTTParty.
+  # Open/Closed Principle:
+  # Provides a hook for potential extension by handling HTTParty errors.
+  def self.make_request(url)
+    HTTParty.get(url)
+  rescue HTTParty::Error, StandardError => e
+    # Handle HTTParty errors and other StandardErrors here
+    puts "An error occurred: #{e.message}"
+    nil
+  end
+
+  # Single Responsibility Principle:
+  # Parses the API response and returns the forecast data.
+  # Open/Closed Principle:
+  # Provides a hook for potential extension by handling different response scenarios.
+  def self.parse_response(response)
+    return nil unless response&.success?
+
+    begin
+      json = JSON.parse(response.body)
+    rescue JSON::ParserError => e
+      # Handle JSON parsing errors here
+      puts "JSON parsing error: #{e.message}"
+      return nil
     end
+
+    json
+  end
 end
+
+# Comments on Developer Happiness:
+# Developers are likely to find this class more enjoyable to work with due to its clean and modular design.
+# The separation of concerns and the use of well-named methods make the codebase more readable.
+# Additionally, the provided hooks for extension allow developers to implement changes without the fear of breaking existing functionality.
